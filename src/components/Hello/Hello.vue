ffa534897e56bb4a05dfe939fb00b5ad782fee1b
leofle <leofle@gmail.com>
2018-10-07 00:24:25 +0300

    adding styles2

--- src/components/Hello/Hello.vue
+++ src/components/Hello/Hello.vue
@@ -16,14 +16,9 @@
 </template>
 
 <script>
-import styled from 'vue-styled-components'
-import Form from '../Form/Form'
-import Donut from '../Donut/DonutChart'
-
-const Container = styled.div`
-	display:flex;
-	justify-content: center;
-`;
+import styled from "vue-styled-components";
+import Form from "../Form/Form";
+import Donut from "../Donut/DonutChart";
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -31,43 +26,45 @@ const Title = styled.h1`
   color: palevioletred;
 `;
 const CardFlex = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	word-break: break-word;
-	margin: 24px;
-	padding: 16px;
-	color: #757575;
-	border-radius: 5px;
-	background-color: #fff;
-	box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  word-break: break-word;
+  margin: 24px;
+  padding: 16px;
+  color: #757575;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
+    0 3px 1px -2px rgba(0, 0, 0, 0.2);
 `;
 
 const Card = styled.div`
-	margin: 24px;
-	padding: 16px;
-	color: #757575;
-	border-radius: 5px;
-	background-color: #fff;
-	box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+  margin: 24px;
+  padding: 16px;
+  color: #757575;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
+    0 3px 1px -2px rgba(0, 0, 0, 0.2);
 `;
 
 const DonutContainer = styled.div`
-		svg {
-			margin: auto;
-			display: inherit;
-		}
+  svg {
+    margin: auto;
+    display: inherit;
+  }
 `;
 
-  export default {
-		name: 'Hello',
-		components: {
-			Card,
-			CardFlex,
-			DonutContainer,
-			Title,
-			Form,
-			Donut
-  	},
+export default {
+  name: "Hello",
+  components: {
+    Card,
+    CardFlex,
+    DonutContainer,
+    Title,
+    Form,
+    Donut
   }
+};
 </script>
